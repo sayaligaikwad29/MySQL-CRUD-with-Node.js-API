88241e7dd02867275f3c067fbbac3f4315c73cf0
Sayali Gaikwad <Sayali.Gaikwad@harbingergroup.com>
2024-05-29 15:17:50 +0530

    Kubernetes script added

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql2/promise')
 
 const mysqlPool = mysql.createPool({
-    host: 'db',
-    user: 'admin',
-    password: 'root',
-    database: 'demo'
+    // host: 'db',
+    // user: 'admin',
+    // password: 'root',
+    // database: 'demo'
+
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 })
 
 
